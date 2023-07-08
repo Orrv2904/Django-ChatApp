22680a1358e42250600f88d13fa1fc080a6c2e57
Orrv2904 <Omarrv2904@gmail.com>
2023-07-08 15:52:42 -0600

    running

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def home(request):
@@ -8,7 +9,7 @@ def home(request):
 def room(request, room):
     username = request.GET.get('username')
     room_details = Room.objects.get(name=room)
-    return render(request, 'room.html', {
+    return render(request, 'chat/room.html', {
         'username': username,
         'room': room,
         'room_details': room_details
@@ -24,3 +25,18 @@ def checkview(request):
         new_room = Room.objects.create(name=room)
         new_room.save()
         return redirect('/'+room+'/?username='+username)
+    
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
